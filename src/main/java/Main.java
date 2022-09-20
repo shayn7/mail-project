7b189d2df28b8d1397eb6f5e3e9f897687ce0acb
shayn7 <53445875+shayn7@users.noreply.github.com>
2022-09-20 22:51:13 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,26 +12,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-            performTask();
+            
     }
 
-    private static void performTask() {
-        EmailReader emailReader = new EmailReader();
-        EmailSender emailSender = new EmailSender();
-        PythonHandler pythonHandler = new PythonHandler();
-        IOException ioException = null;
-        emailReader.readMail("md7343@gmail.com", "dejzhnsvaucxivih");
-        if(emailReader.getText().contains("banana")){
-            try {
-                String output = pythonHandler.runPythonScript("/Users/shay/mail-project/src/main/resources/main.py");
-                emailSender.sendMessage("md7343@gmail.com", "dejzhnsvaucxivih", "md7343@gmail.com", output);
-            } catch (IOException e) {
-                LOG.error("failed to run the python file");
-                ioException = e;
-            }
-        } else emailSender.sendMessage("md7343@gmail.com", "dejzhnsvaucxivih", "md7343@gmail.com", "Invalid keyword");
-        
-        if(!emailReader.isHasAttachments()) emailSender.sendMessage("md7343@gmail.com", "dejzhnsvaucxivih", "md7343@gmail.com", "Attachment missing");
-        if(ioException != null) emailSender.sendMessage("md7343@gmail.com", "dejzhnsvaucxivih", "md7343@gmail.com", String.valueOf(ioException));
-    }
 }
